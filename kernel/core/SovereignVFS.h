a79ff5c4ba8e141c30fedf3ce15503964fbe2572
Sovereign-User <sovereign@users.noreply.github.com>
2026-04-09 00:03:03 +0530

    fix(ci): Purged rogue stdint.h dependency in SovereignVFS.h to enforce Absolute Purity Audit

--- kernel/core/SovereignVFS.h
+++ kernel/core/SovereignVFS.h
@@ -1,7 +1,7 @@
 #ifndef SOVEREIGN_VFS_H
 #define SOVEREIGN_VFS_H
 
-#include <stdint.h>
+#include "../../include/SovereignLibC.h"
 
 /*
  * Σ SIGMAOS: VIRTUAL FILE SYSTEM ABSTRACTION
@@ -10,20 +10,20 @@
 
 struct vfs_node;
 
-typedef uint64_t (*vfs_read_func)(struct vfs_node* node, uint64_t offset, uint64_t size, uint8_t* buffer);
-typedef uint64_t (*vfs_write_func)(struct vfs_node* node, uint64_t offset, uint64_t size, uint8_t* buffer);
+typedef sigma_u64 (*vfs_read_func)(struct vfs_node* node, sigma_u64 offset, sigma_u64 size, sigma_u8* buffer);
+typedef sigma_u64 (*vfs_write_func)(struct vfs_node* node, sigma_u64 offset, sigma_u64 size, sigma_u8* buffer);
 typedef void (*vfs_open_func)(struct vfs_node* node);
 typedef void (*vfs_close_func)(struct vfs_node* node);
 
 typedef struct vfs_node {
     char name[128];
-    uint32_t mask;
-    uint32_t uid;
-    uint32_t gid;
-    uint32_t flags;
-    uint32_t inode;
-    uint32_t length;
-    uint32_t impl; // implementation-dependent descriptor
+    sigma_u32 mask;
+    sigma_u32 uid;
+    sigma_u32 gid;
+    sigma_u32 flags;
+    sigma_u32 inode;
+    sigma_u32 length;
+    sigma_u32 impl; // implementation-dependent descriptor
 
     vfs_read_func read;
     vfs_write_func write;
