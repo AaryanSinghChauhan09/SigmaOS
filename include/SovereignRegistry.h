eea27bb57a26f0622522b0ed2ed94dd8d2474f20
Sovereign-User <sovereign@users.noreply.github.com>
2026-04-15 22:40:16 +0530

    fix: Add SovereignRegistry.h proxy to clear IDE ghost errors

---
+++ include/SovereignRegistry.h
@@ -0,0 +1,7 @@
+#ifndef ROOT_SOVEREIGN_REGISTRY_H
+#define ROOT_SOVEREIGN_REGISTRY_H
+
+/* Proxy header to clear IDE caches and forward appropriately */
+#include "suites/S10_Registry/shards/SovereignRegistry.h"
+
+#endif /* ROOT_SOVEREIGN_REGISTRY_H */
