1c2a4978cff8902a9f2bba9e0ea592340b8ffd25
Aaryan Singh Chauhan <aaryansinghchauhan@sigmaos.com>
2026-04-22 11:56:03 +0530

    Sigma Hardening: Achieved industrial-grade modular root structure

--- scripts/modules/02_globals.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    window.guiView = document.getElementById('gui-view');
-    window.cliView = document.getElementById('cli-view');
-    window.bootOverlay = document.getElementById('boot-overlay');
-});
