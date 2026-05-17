a2a0cfa4efa71cb04e9ecc42601470e4cfce3bb5
Sovereign-User <sovereign@users.noreply.github.com>
2026-05-17 11:54:11 +0530

    Eradicate duplicate definitions in headers causing redefinition errors

--- include/sigma_boot.h
+++ include/sigma_boot.h
@@ -6,12 +6,6 @@
 extern "C" {
 #endif
 
-typedef enum { 
-    SIGMA_BOOT_STAGE_INIT, 
-    SIGMA_BOOT_STAGE_KERNEL, 
-    SIGMA_BOOT_STAGE_USERLAND, 
-    SIGMA_BOOT_STAGE_RECOVERY 
-} sigma_boot_stage_t;
 
 #define SIGMA_BOOT_GENESIS 0
 #define SIGMA_BOOT_LATTICE_IGNITION 1
