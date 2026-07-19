59c04256811c0f5abf59b584ea3ddd4a5db7b687
SigmaOS Development <dev@sigmaos.org>
2026-07-19 16:08:54 +0530

    Implement file manager (100-Ideas #21) with view modes, sorting, and bookmarks

--- src/filesystem/mod.rs
+++ src/filesystem/mod.rs
@@ -1,4 +1,9 @@
 // SigmaOS Filesystem Module
+pub mod manager;
 pub mod vfs;
 
+pub use manager::{
+    ClipboardOperation, FileItem, FileManager, FileManagerError, FileOperation, FileType as ManagerFileType,
+    SortOrder, StandardFileOperation, ViewMode,
+};
 pub use vfs::{FileDescriptor, FilePermissions, FileType, FsError, Inode, VirtualFilesystem};
